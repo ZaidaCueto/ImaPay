3b6cb305619145db60e612fe5fc2d831d96059e8
Jonas Narvas Balbino <JNB.dev@outlook.com>
2023-04-22 12:36:20 -0300

    Adding files to back end

--- ImaPay_BackEnd/ImaPay_BackEnd/Domain/Dtos/TransactionDto.cs
+++ ImaPay_BackEnd/ImaPay_BackEnd/Domain/Dtos/TransactionDto.cs
@@ -3,9 +3,10 @@
     public class TransactionDto
     {
         public int Amount { get; set; }
-        
         public DateTime Date { get; set; }
-
         public int Sender { get; set; }
+        public int Reciever { get; set; }
+        public int AccountId { get; set; }
+
     }
 }
---
+++ ImaPay_BackEnd/ImaPay_BackEnd/Repositories/AccountRepository.cs
@@ -0,0 +1,24 @@
+﻿using ImaPay_BackEnd.Domain;
+using ImaPay_BackEnd.Domain.Model;
+
+namespace ImaPay_BackEnd.Repositories;
+
+public class AccountRepository
+{
+    private readonly BankContext _bank;
+
+    public AccountRepository(BankContext context)
+    {
+        _bank = context;
+    }
+
+    private List<Transaction> _getAll()
+    {
+        return _bank.Transactions.ToList();
+    }
+
+    public Transaction GetAllTransactionsById(int id)
+    {
+        return _getAll().Find(transaction => transaction.Id.Equals(id));
+    }
+}
